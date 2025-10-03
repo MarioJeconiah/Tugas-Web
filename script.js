77392dc14f8396b1dfc23bf9d58ac183cb1ec3dc
MarioJeconiah <mariopurba40@gmail.com>
2025-10-03 07:31:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const nama = prompt("Masukkan nama anda");
+const Hobi = prompt("Masukkan hobi anda");
+const tentangKamu = prompt("Jelaskan apa saja tentang kamu");
+
+document.getElementById("user").innerHTML +=
+  "<h3><b>Nama user:</b> " + nama + "</h3>" +
+  "<p><b>Hobi:</b> " + Hobi + "</p>" +
+  "<p>" + tentangKamu + "</p>" ;
+
+let sesuatu = "asep";
+
+while (true) {
+document.getElementsByClassName("asep").innerHTML += 
+sesuatu;
+}
